72a4af789364b88442d9e2a14203b80868792a95
Cho-Ik-Jun <harry960629@naver.com>
2023-10-04 15:33:22 +0900

    feat: 게임 보드판 생성 로직 구현

---
+++ tetris.js
@@ -0,0 +1,19 @@
+const board = document.querySelector('.playground > ul');
+
+// 변수
+const BOARD_ROWS = 20;
+const BOARD_COLS = 10;
+// 보드에 줄 추가
+for (let i = 0; i < BOARD_ROWS; i++) {
+  const boardNewLine = document.createElement('li'); // 가로 한줄
+  const boardNewLineContainer = document.createElement('ul'); // 가로 한줄 컨테이너
+  // 가로 갯수만큼 박스 생성 후 컨테이너에 부착
+  for (let j = 0; j < BOARD_COLS; j++) {
+    const boardNewBox = document.createElement('li');
+    boardNewLineContainer.prepend(boardNewBox);
+  }
+  // 가로 한줄에 부착
+  boardNewLine.prepend(boardNewLineContainer);
+  // 최종 보드에 한줄 추가
+  board.prepend(boardNewLine);
+}
