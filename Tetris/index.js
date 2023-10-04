08f1c77ee8e3dbe28f753f2654c228d86d9dba53
SoJuSo <asdfg7123@gmail.com>
2023-10-04 16:29:22 +0900

    Change: CSS파일을 제외한 html, js를 재구성하기 위해 기존 소스 삭제

--- Tetris/index.js
+++ Tetris/index.js
@@ -1,15 +1,1 @@
 const $ = document;
-
-const playground = $.querySelector(".playground ul");
-console.log(playground);
-
-for (let i = 0; i < 20; i++) {
-  const li = $.createElement("li");
-  const ul = $.createElement("ul");
-  for (let j = 0; j < 10; j++) {
-    const matrix = $.createElement("li");
-    ul.prepend(matrix);
-  }
-  li.prepend(ul);
-  playground.prepend(li);
-}
