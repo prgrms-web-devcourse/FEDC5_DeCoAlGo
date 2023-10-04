ac01ccc23eabaaecc41fd2d04594da2a0e93b986
SoJuSo <asdfg7123@gmail.com>
2023-10-04 16:54:43 +0900

    Feat: 보드 기능 구현 완료 Comment: html을 주로 사용하기보다는 js에서 추가하는 형식으로 구현했고 기존에 class형태를 id형태로 바꿔서 구현했습니다.

--- Tetris/index.js
+++ Tetris/index.js
@@ -1,1 +1,27 @@
 const $ = document;
+
+const score = $.createElement("div"); //점수
+score.id = "score";
+score.innerText = 0;
+
+const board = $.createElement("div"); //보드
+board.id = "board";
+
+const whole_ul = $.createElement("ul"); //전체를 가지고 있는 ul
+
+$.getElementById("wrapper").appendChild(score);
+$.getElementById("wrapper").appendChild(board);
+$.getElementById("board").appendChild(whole_ul);
+
+// console.log(whole_ul);
+
+for (let i = 0; i < 20; i++) {
+  const li = $.createElement("li"); // 가로 행 한 줄을 가지고 있는 li tag
+  const ul = $.createElement("ul"); // 가로 행 한 줄
+  for (let j = 0; j < 10; j++) {
+    const matrix = $.createElement("li"); //각각 하나의 칸
+    ul.prepend(matrix);
+  }
+  li.prepend(ul);
+  whole_ul.prepend(li);
+}
