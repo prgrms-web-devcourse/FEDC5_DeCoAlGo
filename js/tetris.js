ec00069ba707355a50f7357e873c35ab0d6be387
nimgnas <sangminyn@gmail.com>
2023-10-04 18:00:31 +0900

    feat: 보드 기본 렌더링, 스타일 작업

--- js/tetris.js
+++ js/tetris.js
@@ -1,1 +1,27 @@
-console.log("dd");
+// DOM
+const playground = document.querySelector(".playground > ul");
+
+const GAME_ROWS = 20;
+const GAME_COLS = 10;
+
+init();
+
+// functions
+function init() {
+  for (let i = 0; i < GAME_ROWS; i++) {
+    prependNewLine();
+  }
+}
+
+function prependNewLine() {
+  const li = document.createElement("li");
+  const ul = document.createElement("ul");
+
+  for (let j = 0; j < GAME_COLS; j++) {
+    const matrix = document.createElement("li");
+    ul.prepend(matrix);
+  }
+
+  li.prepend(ul);
+  playground.prepend(li);
+}
