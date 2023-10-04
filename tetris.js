8819a07f29457ae74ff149e75e1f954ae5516b64
Cho-Ik-Jun <harry960629@naver.com>
2023-10-04 15:37:41 +0900

    refactor: 보드 줄 추가 함수 분리 및 초기화 함수 추가

--- tetris.js
+++ tetris.js
@@ -3,8 +3,19 @@ const board = document.querySelector('.playground > ul');
 // 변수
 const BOARD_ROWS = 20;
 const BOARD_COLS = 10;
-// 보드에 줄 추가
-for (let i = 0; i < BOARD_ROWS; i++) {
+
+init(); // 초기화
+
+// 함수
+// 시작 초기화 함수
+function init() {
+  // 보드에 모든 줄 추가
+  for (let i = 0; i < BOARD_ROWS; i++) {
+    addNewLine();
+  }
+}
+// 한 줄 추가 함수
+function addNewLine() {
   const boardNewLine = document.createElement('li'); // 가로 한줄
   const boardNewLineContainer = document.createElement('ul'); // 가로 한줄 컨테이너
   // 가로 갯수만큼 박스 생성 후 컨테이너에 부착
