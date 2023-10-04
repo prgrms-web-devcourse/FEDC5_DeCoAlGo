4a5880390bf04348e81efaf75c03b188b1da3069
yejinleee <yejin9487@gmail.com>
2023-10-04 17:42:32 +0900

    feat: 다음 블럭용 미니보드판 추가

--- js/tetris.js
+++ js/tetris.js
@@ -5,8 +5,21 @@ for (let i = 0; i < 20; i++) {
   const li = document.createElement("li");
   for (let j = 0; j < 10; j++) {
     const cell = document.createElement("li");
+    ul.prepend(cell);
+  }
+  li.prepend(ul);
+  playground.prepend(li);
+}
+
+const miniground = document.querySelector(".miniground > ul");
+
+for (let i = 0; i < 5; i++) {
+  const ul = document.createElement("ul");
+  const li = document.createElement("li");
+  for (let j = 0; j < 5; j++) {
+    const cell = document.createElement("li");
     ul.appendChild(cell);
   }
   li.appendChild(ul);
-  playground.appendChild(li);
+  miniground.appendChild(li);
 }
